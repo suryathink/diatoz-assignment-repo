05d89c0ee048f7fc2b7f62d2271f85b15d2a03f6
Surya Prakash Singh <suryaprakashsingh110@gmail.com>
2023-07-22 12:53:05 +0530

    Update Login.jsx

--- client/my-app/src/Components/Login.jsx
+++ client/my-app/src/Components/Login.jsx
@@ -1,99 +1,112 @@
-import  React,{useState,useContext} from 'react';
-import { toast } from 'react-toastify';
-import  {useNavigate} from "react-router-dom"
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { ctx } from './Context/AuthContext';
-
+import React, { useState, useContext } from "react";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { ctx } from "./Context/AuthContext";
+import CircularProgress from "@mui/material/CircularProgress";
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
-
 const defaultTheme = createTheme();
 
 export default function SignIn() {
-  const [nameState, setNameState] = useState("");
-  const [emailState, setEmailState] = useState("")
-  const {setIsAuth} = useContext(ctx)
+  const [passwordState, setPasswordState] = useState("");
+  const [emailState, setEmailState] = useState("");
+  const [emailError, setEmailError] = useState(false);
+  const { setIsAuth } = useContext(ctx);
+  const [loading, setLoading] = useState(false);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    loginUser(data.get('email'),data.get('password'))
+    loginUser(emailState, passwordState);
 
-    setNameState(nameState)
-    setEmailState(emailState)
+    // setNameState(nameState)
+    // setEmailState(emailState)
     console.log({
-      email: data.get('email'),
+      email: data.get("email"),
       // password: data.get('password'),
     });
   };
-   
+
   async function loginUser(email, password) {
-    const apiUrl = 'https://pantyhose-dugong.cyclic.app/login'; 
-  
+    const apiUrl = "https://pantyhose-dugong.cyclic.app/login";
+
     try {
+      setLoading(true);
       const response = await fetch(apiUrl, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          email: email+"",
-          password: password+"",
+          email: email + "",
+          password: password + "",
         }),
       });
       const userData = await response.json();
 
       if (!userData.ok) {
-        console.log("Response",userData)
+        console.log("Response1", userData);
         // If the response status is not in the range 200-299, it means an error occurred
         // console.log("UserData Coming from Backend",userData.error)
         // const errorData = await response.json();
-        toast.error(userData.error)
+        toast.error(userData.error);
       }
-  
-   
+
       // console.log("UserData Coming from Backend",userData)
-      toast.success(userData.message)
-      localStorage.setItem("token",userData.data.data.token)
-      setIsAuth(true)
-      navigate("/")
-    // console.log(userData.data.data.token)
-      return userData; 
-  
+      toast.success(userData.message);
+      localStorage.setItem("token", userData.data.data.token);
+      setIsAuth(true);
+      navigate("/");
+      // console.log(userData.data.data.token)
+      return userData;
     } catch (error) {
-      console.error('Error logging in:', error);
-      toast.error("Something Went Wrong",error)
+      console.error("Error logging in:", error);
+      // toast.error("Something Went Wrong",error)
 
       // throw new Error('Login failed. Please try again.'); // Generic error message for any login failure
+    } finally {
+      setLoading(false); // Stop loading
     }
   }
-  
-
+  const handleEmailChange = (e) => {
+    const email = e.target.value;
+    // Regular expression to validate email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValidEmail = emailRegex.test(email);
+    setEmailError(!isValidEmail);
+    setEmailState(email);
+  };
   return (
     <ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
@@ -101,18 +114,23 @@ export default function SignIn() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
             <TextField
               margin="normal"
               required
@@ -120,6 +138,11 @@ export default function SignIn() {
               id="email"
               label="Email Address"
               name="email"
+              type="email"
+              onChange={handleEmailChange}
+              value={emailState}
+              error={emailError}
+              helperText={emailError ? "Please provide Email address only" : ""}
               autoComplete="email"
               autoFocus
             />
@@ -131,33 +154,35 @@ export default function SignIn() {
               label="Password"
               type="password"
               id="password"
+              onChange={(e) => setPasswordState(e.target.value)}
               autoComplete="current-password"
             />
-            {/* <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            /> */}
+
             <Button
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              disabled={
+                emailState === "" ||
+                passwordState === "" ||
+                emailError == true ||
+                loading
+              }
             >
-              Sign In
+              {loading ? <CircularProgress size={20} /> : "Sign In"}
             </Button>
             <Grid container>
-              {/* <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid> */}
               <Grid item>
-              <p style={{color:"#0079FF",cursor:"pointer"}}  onClick={()=>{navigate("/signup")}}  variant="body2">
-               Don't have an account? Sign Up
+                <p
+                  style={{ color: "#0079FF", cursor: "pointer" }}
+                  onClick={() => {
+                    navigate("/signup");
+                  }}
+                  variant="body2"
+                >
+                  Don't have an account? Sign Up
                 </p>
-                {/* <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link> */}
               </Grid>
             </Grid>
           </Box>
@@ -166,3 +191,8 @@ export default function SignIn() {
     </ThemeProvider>
   );
 }
+/*
+disabled={loading}
+
+
+*/
