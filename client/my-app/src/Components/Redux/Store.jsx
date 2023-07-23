0bda0488851bd2090325823176b2f8ed9b7acd33
Surya Prakash Singh <suryaprakashsingh110@gmail.com>
2023-07-23 20:18:24 +0530

    Update Store.jsx

--- client/my-app/src/Components/Redux/Store.jsx
+++ client/my-app/src/Components/Redux/Store.jsx
@@ -3,6 +3,6 @@ import myReducer from "./Reducer";
 import logger from "redux-logger"; 
 
 
-const myStore = createStore(myReducer, applyMiddleware(logger));
+const myStore = createStore(myReducer);
 
 export default myStore;
